9a35f8916a1bd2ee15e28fe5ab422e249f9adea9
yifty08 <yifty08@icloud.com>
2019-03-15 17:01:15 -0400

    step one done

--- src/main/java/com/example/bullhorn/HomeController.java
+++ src/main/java/com/example/bullhorn/HomeController.java
@@ -4,9 +4,11 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Map;
 
@@ -30,10 +32,14 @@ public class HomeController {
         return "form";
     }
 
-    @PostMapping("/add")
-    public String processActor(@ModelAttribute Message message, @RequestParam("file") MultipartFile file) {
+    @PostMapping("/process")
+    public String processActor(@Valid Message message, BindingResult result,
+                               @RequestParam("file") MultipartFile file) {
+        if (result.hasErrors()) {
+            return  "form";
+        }
         if (file.isEmpty()) {
-            return "redirect:/add";
+            return "redirect:/form";
         }
         try {
             Map uploadResult = cloudc.upload(file.getBytes(),
@@ -42,8 +48,26 @@ public class HomeController {
             messageRepository.save(message);
         } catch (IOException e) {
             e.printStackTrace();
-            return "redirect:/add";
+            return "redirect:/form";
         }
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "form";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id) {
+        messageRepository.deleteById(id);
         return "redirect:/";
     }
 }
